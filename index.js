fcb5f50727e79b59e3f5e61a0ad26f622ce4ef7e
Glitch (api-inotebook) <none>
2022-06-17 05:42:13 +0000

    🏠⚽️ Checkpoint

--- index.js
+++ index.js
@@ -4,21 +4,11 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
-const { init } = require("glitch-deploy-helper");
 
-async function main() {
-  await init();
-}
-
-main();
 // Connection to DB
 connectToMongo();
 //Avilable Routes
-app.use(
-  cors({
-    origin: "https://app-inotebook.vercel.app",
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
