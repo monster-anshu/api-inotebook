9cd613ceae77a97d7d21d9e58851ed9b728cda4d
monster anshu <hgunwant2312@gmail.com>
2022-06-17 10:55:50 +0530

    v1.0

---
+++ index.js
@@ -0,0 +1,27 @@
+const connectToMongo = require("./db");
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+// Connection to DB
+connectToMongo();
+//Avilable Routes
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
+app.get("/api", (req, res) => {
+  res.send("Hello Himanshu");
+});
+app.get("/", (req, res) => {
+  res.send("Api working updated 2");
+});
+// app.use(express.static(path.join(__dirname, "../build")));
+// app.get("/*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "../build", "index.html"));
+// });
+app.listen(port, () => {
+  console.log(`Example app listening at PORT = ${port}`);
+});
